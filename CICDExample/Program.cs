14fd32e00806cc7fbae0e95af701d9dc7b478a35
Matthias933 <matthias@bachmann.info>
2024-11-28 11:30:08 +0100

    initial commit

---
+++ CICDExample/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace CICDExample
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        public static int TestMe(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
